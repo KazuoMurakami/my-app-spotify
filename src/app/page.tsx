e2b4755cdc92f7d1e8812b825b528aca82574c51
Octavio Kazuo <otaviokazuo12@hotmail.com>
2023-07-03 19:28:24 -0300

    aside update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import {Home as HomeIcon, Search, Library, Volume, Plus, ChevronRight} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col mx-2">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-800/40 p-4 rounded">
+        <aside className="w-80 bg-zinc-800/40 p-2 rounded flex flex-col">
             <nav className="flex flex-col gap-8 p-4 mb-4">
                     <a href="" className="flex gap-3  hover:text-zinc-100 ease-out duration-500"><HomeIcon/>Inicio</a>
                     <a href="" className="flex gap-3 hover:text-zinc-100 ease-out duration-500"><Search/>Buscar</a>
@@ -15,6 +16,16 @@ export default function Home() {
                     <a href=""  className="hover:text-zinc-100 ease-out duration-500"><Plus/></a>
                     <a href="" className="hover:text-zinc-100 ease-out duration-500"><ChevronRight/></a>
                 </div>
+            </nav>
+            <nav className="flex flex-col flex-1 ">
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
+              <a href="" className="flex-1 items-center flex"> <Image src="/images/coldplay.jpeg" alt="album coldplay" width={50} height={50}/> Playlist</a>
             </nav>
         </aside>
         <main className="flex-1 p-4 bg-gradient-to-b from-green-900 from-10% to-zinc-800/40 to-30% ml-2 rounded">
